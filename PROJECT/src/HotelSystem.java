ec2b2bd9958adc5b10f312c19035f31f77f550ce
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-05-19 23:48:42 -0400

    Hotel System

--- PROJECT/src/HotelSystem.java
+++ PROJECT/src/HotelSystem.java
@@ -6,7 +6,57 @@ import java.io.*;
 import java.util.Scanner;
 
 public class HotelSystem {
+
+    //GLOBAL CONSTANTS
+    static final int ACCOUNT_ID_INDEX = 0;
+    static final int ACCOUNT_FIRSTNAME_INDEX = 1;
+    static final int ACCOUNT_LASTNAME_INDEX = 2;
+    static final int ACCOUNT_PIN_INDEX = 3;
+    static final int ACCOUNT_ENTRY_LENGTH = 4;
+
     public static void main(String[] args){
 
+        //CONSTANTS
+        final String ACCOUNT_FILE = "account.txt";
+        final String RESERVATION_FILE = "reservation.txt";
+
+        //Variables
+        int userID;
+        boolean isAdmin = false;
+        String [][] roomData, accountData;
+    }
+
+    public static String[][] readAccounts(String filepath) {
+
+        //Declarations
+        int numOfLines = 0;
+        String[][] data;
+        BufferedReader file;
+
+        try {
+
+            //COUNT NUM OF LINES
+            for(file = new BufferedReader(new FileReader(filepath)); file.readLine() != null; ++numOfLines) {
+
+            }
+
+            System.out.println(numOfLines);
+
+            //INIT DATA[][] and reset BufferedReader to top of file.
+            data = new String[numOfLines][ACCOUNT_ENTRY_LENGTH];
+            file = new BufferedReader(new FileReader(filepath));
+
+            //Read File, Fill out Data
+            for(int i = 0; i < data.length; ++i) {
+                data[i] = file.readLine().split(",");
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println(e + "\nUH OH! YOU CAN'T READ THE FILE!");
+            data = null;
+        }
+
+        return data;
     }
 }
