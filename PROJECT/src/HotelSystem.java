1f7e8334fef2c6bdb4672177e0e909c8cc0bf9b8
Ryan Mehrian <ryan.mehrian@gmail.com>
2024-05-20 00:42:47 -0400

    finished readRoomInfo()
    !

--- PROJECT/src/HotelSystem.java
+++ PROJECT/src/HotelSystem.java
@@ -3,7 +3,6 @@ djfjsjgsfjkj
 */
 
 import java.io.*;
-import java.util.Scanner;
 
 public class HotelSystem {
 
@@ -12,7 +11,13 @@ public class HotelSystem {
     static final int ACCOUNT_FIRSTNAME_INDEX = 1;
     static final int ACCOUNT_LASTNAME_INDEX = 2;
     static final int ACCOUNT_PIN_INDEX = 3;
-    static final int ACCOUNT_ENTRY_LENGTH = 4;
+    static final int ACCOUNT_ELEMENT_COUNT = 4;
+    static final int ROOM_NUMBER_INDEX = 0;
+    static final int RESERVATION_DATE_INDEX = 1;
+    static final int CUSTOMER_FIRSTNAME_INDEX = 2;
+    static final int CUSTOMER_LASTNAME_INDEX = 3;
+    static final int REGISTRAR_ID_INDEX = 4;
+    static final int RESERVATION_ELEMENT_COUNT = 5;
 
     public static void main(String[] args){
 
@@ -27,12 +32,21 @@ public class HotelSystem {
 
         //TESTING (delete later)
         accountData = readAccounts(ACCOUNT_FILE);
+        roomData = readRoomInfo(RESERVATION_FILE);
+
         for(int i = 0; i < accountData.length; i++){
             for(int j = 0; j < accountData[i].length; j++){
                 System.out.print(accountData[i][j] + " ");
             }
             System.out.println();
         }
+        for(int i = 0; i < roomData.length; i++){
+            for(int j = 0; j < roomData[i].length; j++){
+                System.out.print(roomData[i][j] + " ");
+            }
+            System.out.println();
+        }
+        //END TESTING
 
     }
 
@@ -64,7 +78,7 @@ public class HotelSystem {
             }
 
             //INIT DATA[][] and reset BufferedReader to top of file.
-            data = new String[numOfLines][ACCOUNT_ENTRY_LENGTH];
+            data = new String[numOfLines][ACCOUNT_ELEMENT_COUNT];
             file.close();
             file = new BufferedReader(new FileReader(filepath));
 
@@ -76,7 +90,54 @@ public class HotelSystem {
 
         }
         catch (IOException e) {
-            System.out.println(e + "\nUH OH! YOU CAN'T READ THE FILE!");
+            System.out.println(e + "\nUH OH! YOU CAN'T READ THE ACCOUNT ID FILE!");
+            data = null;
+        }
+
+        return data;
+    }
+
+    /*
+    Method: readRoomInfo
+    -----
+    Parameters:
+    String filepath - The filepath of the reservations file.
+    -----
+    Returns:
+    String[][] data - 2D array that holds all reservation and room info.
+    -----
+    Description: this method reads in all reservation and room info from the file, and
+    then returns it as a 2D array. Each row of the array is a separate reservation, and each column
+    is a piece of info about that reservation (Room Number, Check-In Date, Check-Out Date, Guest Name)
+    */
+    public static String[][] readRoomInfo(String filepath) {
+
+        //Declarations
+        int numOfLines = 0;
+        String[][] data;
+        BufferedReader file;
+
+        try {
+
+            //COUNT NUM OF LINES
+            for(file = new BufferedReader(new FileReader(filepath)); file.readLine() != null; ++numOfLines) {
+                //ALL THE COUNTING IS DONE IN THE HEADER, NO BODY REQUIRED!
+            }
+
+            //INIT DATA[][] and reset BufferedReader to top of file.
+            data = new String[numOfLines][RESERVATION_ELEMENT_COUNT];
+            file.close();
+            file = new BufferedReader(new FileReader(filepath));
+
+            //Read File, Fill out Data
+            for(int i = 0; i < data.length; ++i) {
+                data[i] = file.readLine().split(",");
+            }
+            file.close();
+
+        }
+        catch (IOException e) {
+            System.out.println(e + "\nUH OH! YOU CAN'T READ THE RESERVATIONS FILE!");
             data = null;
         }
 
